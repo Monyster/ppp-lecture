9e5f9a02ab48d9a8f990a37d7bce98eab4851f1a
Matvey V <main.monyster@gmail.com>
2023-08-01 20:24:17 +0300

    * prettify

--- client/src/pages/Workspace.tsx
+++ client/src/pages/Workspace.tsx
@@ -1,8 +1,4 @@
-import type {
-  DraggableLocation,
-  DroppableProvided,
-  DropResult,
-} from '@hello-pangea/dnd';
+import type { DraggableLocation, DroppableProvided, DropResult } from '@hello-pangea/dnd';
 import { DragDropContext, Droppable } from '@hello-pangea/dnd';
 import React, { useContext, useEffect, useState } from 'react';
 
@@ -15,80 +11,72 @@ import { reorderService } from '../services/reorder.service';
 import { Container } from './styled/container';
 
 export const Workspace = () => {
-  const [lists, setLists] = useState<List[]>([]);
+    const [lists, setLists] = useState<List[]>([]);
 
-  const socket = useContext(SocketContext);
+    const socket = useContext(SocketContext);
 
-  useEffect(() => {
-    socket.emit(ListEvent.GET, (lists: List[]) => setLists(lists));
-    socket.on(ListEvent.UPDATE, (lists: List[]) => setLists(lists));
+    useEffect(() => {
+        socket.emit(ListEvent.GET, (lists: List[]) => setLists(lists));
+        socket.on(ListEvent.UPDATE, (lists: List[]) => setLists(lists));
 
-    return () => {
-      socket.removeAllListeners(ListEvent.UPDATE).close();
+        return () => {
+            socket.removeAllListeners(ListEvent.UPDATE).close();
+        };
+    }, []);
+
+    const onDragEnd = (result: DropResult) => {
+        if (!result.destination) {
+            return;
+        }
+
+        const source: DraggableLocation = result.source;
+        const destination: DraggableLocation = result.destination;
+
+        const isNotMoved = source.droppableId === destination.droppableId && source.index === destination?.index;
+
+        if (isNotMoved) {
+            return;
+        }
+
+        const isReorderLists = result.type === 'COLUMN';
+
+        if (isReorderLists) {
+            setLists(reorderService.reorderLists(lists, source.index, destination.index));
+            socket.emit(ListEvent.REORDER, source.index, destination.index);
+
+            return;
+        }
+
+        setLists(reorderService.reorderCards(lists, source, destination));
+        socket.emit(CardEvent.REORDER, {
+            sourceListId: source.droppableId,
+            destinationListId: destination.droppableId,
+            sourceIndex: source.index,
+            destinationIndex: destination.index,
+        });
     };
-  }, []);
 
-  const onDragEnd = (result: DropResult) => {
-    if (!result.destination) {
-      return;
-    }
-
-    const source: DraggableLocation = result.source;
-    const destination: DraggableLocation = result.destination;
-
-    const isNotMoved =
-      source.droppableId === destination.droppableId &&
-      source.index === destination?.index;
-
-    if (isNotMoved) {
-      return;
-    }
-
-    const isReorderLists = result.type === 'COLUMN';
-
-    if (isReorderLists) {
-      setLists(
-        reorderService.reorderLists(lists, source.index, destination.index),
-      );
-      socket.emit(ListEvent.REORDER, source.index, destination.index);
-
-      return;
-    }
-
-    setLists(reorderService.reorderCards(lists, source, destination));
-    socket.emit(CardEvent.REORDER, {
-      sourceListId: source.droppableId,
-      destinationListId: destination.droppableId,
-      sourceIndex: source.index,
-      destinationIndex: destination.index,
-    });
-  };
-
-  return (
-    <React.Fragment>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="board" type="COLUMN" direction="horizontal">
-          {(provided: DroppableProvided) => (
-            <Container
-              className="workspace-container"
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-            >
-              {lists.map((list: List, index: number) => (
-                <Column
-                  key={list.id}
-                  index={index}
-                  listName={list.name}
-                  cards={list.cards}
-                  listId={list.id}
-                />
-              ))}
-              {provided.placeholder}
-              <ColumnCreator onCreateList={() => {}} />
-            </Container>
-          )}
-        </Droppable>
-      </DragDropContext>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable droppableId="board" type="COLUMN" direction="horizontal">
+                    {(provided: DroppableProvided) => (
+                        <Container className="workspace-container" ref={provided.innerRef} {...provided.droppableProps}>
+                            {lists.map((list: List, index: number) => (
+                                <Column
+                                    key={list.id}
+                                    index={index}
+                                    listName={list.name}
+                                    cards={list.cards}
+                                    listId={list.id}
+                                />
+                            ))}
+                            {provided.placeholder}
+                            <ColumnCreator onCreateList={() => {}} />
+                        </Container>
+                    )}
+                </Droppable>
+            </DragDropContext>
+        </React.Fragment>
+    );
 };
